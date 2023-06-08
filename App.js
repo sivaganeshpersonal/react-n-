4d325dd8719d345aa71a04bf244224b1c6e04767
sivaganeshpersonal <sivaganeshnimmadala94@gmail.com>
2023-06-08 23:34:15 +0530

    mostly parcel

--- App.js
+++ App.js
@@ -1,34 +1,5 @@
-// creating react elemet, root and render react element inside root
-// const heading = React.createElement(
-//     "h1",
-//     { id: "heading" },
-//     "Hellow world form react"
-//   );
-  
-//   const root = ReactDOM.createRoot(document.getElementById("root"));
-  
-//   root.render(heading);
-
-//-------------------------------------------------------------------------
-
-//creating nested html structure 
-
-/**
- *
- * <div id="parent">
- *    <div id="child1">
- *       <h1>I'm h1 tag</h1>
- *       <h2>I'm h1 tag</h2>
- *    </div>
- *    <div id="child2">
- *       <h1>I'm h1 tag</h1>
- *       <h2>I'm h1 tag</h2>
- *    </div>
- * </div>
- *
- *
- *
- */
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 const parent = React.createElement(
   "div",
---
+++ README.md
@@ -0,0 +1,22 @@
+# N D React 
+
+# Parcel
+-Dev Build
+-Local server
+-HMR = Hot Module Replacement
+-File Watching Algorithm - written in c++ 
+-caching - Faster Builds 
+-Image Optimization
+-Minification 
+-Bundling
+-Compress
+-Consistant Hashing
+-Code Splitting
+-Differential Bundling - support other browsers
+-Diagnostics
+-Error Handling
+-HTTPS
+-Tree Shaking - remove unused code
+-Different dev and prod bundles
+
+
